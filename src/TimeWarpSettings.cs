432ebab5a2664656568266b0e42832c0d8ac56ab
O'viich <aanton660@gmail.com>
2022-12-03 12:07:18 -0600

    Added source files

---
+++ src/TimeWarpSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityModManagerNet;
+
+namespace TimeWarp
+{
+    public class TimeWarpSettings : UnityModManager.ModSettings
+    {
+        public float GameSpeed = 7f;
+
+        public override void Save(UnityModManager.ModEntry modEntry)
+        {
+            Save(this, modEntry);
+        }
+    }
+}
